4f002c923412437baf42f745f5c686c0fe8b533e
Kris Burke <kris.burke611@gmail.com>
2016-02-12 22:43:28 -0500

    All tests passing.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,6 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  angular.extend($scope, Links);
+  $scope.getLinks();
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,39 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  var data = {};
+  var link = {};
+  
+  var getLinks = function() {
+    return $http({
+      method: 'GET', 
+      url: '/api/links',
+     })
+     .then(function(resp) {
+        console.log(resp);
+        data.links = resp.data;
+     });
+  };
+
+   var addLink = function(){
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      //headers:
+      data: link
+    })
+    .then(function(resp){
+      console.log(resp);
+    })
+   }
+
+  return {
+    data: data,
+    getLinks: getLinks,
+    addLink: addLink,
+    link:link
+  }
+  
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
